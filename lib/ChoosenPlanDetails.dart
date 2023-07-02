b0d82af4b8b89be955a2c078d94e1e4542970bbb
Abdelwahab-Eldesoky <abdelwahabnabil2001@gmail.com>
2023-07-02 03:59:24 +0300

    RankingBoard Done and Gifs Loaded

---
+++ lib/ChoosenPlanDetails.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChoosenPlanDetails extends StatefulWidget {
+  const ChoosenPlanDetails({Key? key}) : super(key: key);
+
+  @override
+  State<ChoosenPlanDetails> createState() => _ChoosenPlanDetailsState();
+}
+
+class _ChoosenPlanDetailsState extends State<ChoosenPlanDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
